a9f84d472245511f9603e39751e426c857451621
Tim Fischbach <tfischbach@codevise.de>
2016-08-11 11:51:37 +0200

    Add compose function to create Resolvers
    Allow creating new resolvers by composing an existing resolver with a
    transform function.

---
+++ js/src/resolvers/compose.js
@@ -0,0 +1,55 @@
+/**
+ * Creates a new Resolver by transforming the results with a function.
+ *
+ * @param {compose~transform} transform
+ *   Function to apply to inner resolver props to get resolved props.
+ *
+ * @param {Class<Resolver>} InnerResolver
+ *   Resolver supplying inputs to the transform function.
+ *
+ * @return {Class<Resolver}
+ *
+ * @example
+ *
+ * resolve.register('pageTitle',
+ *                  compose(getTitle, PageResolver));
+ *
+ * function getTitleLength(pageProps) {
+ *   return pageProps.title;
+ * }
+ *
+ * createContainer(SomeComponent, {
+ *   fragments: {
+ *     linkedPageTitle: resolve('pageTitle', {
+ *       property: 'linkedPageId'
+ *     })
+ *   }
+ * });
+ */
+export default function(transform, InnerResolver) {
+  return function(options, callback) {
+    const inner = new InnerResolver(options, callback);
+
+    return {
+      get(props, seed) {
+        return transform(inner.get(props, seed), seed);
+      },
+
+      dispose() {
+        inner.dispose();
+      }
+    };
+  };
+}
+
+/**
+ * @callback compose~transform
+ *
+ * @param {Object} props
+ *   Result of the inner resolver.
+ *
+ * @param {Object} seed
+ *   Resolver seed data
+ *
+ * @return {Object}
+ */
