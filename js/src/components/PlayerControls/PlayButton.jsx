db5460f29471c66ccae6cf486801ebb844d04359
Tim Fischbach <tfischbach@codevise.de>
2016-08-11 19:12:06 +0200

    Allow using custom play button icon

---
+++ js/src/components/PlayerControls/PlayButton.jsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import Icon from 'components/Icon';
+
+export default function(props) {
+  return (
+    <a className={className(props)}
+       href="#"
+       tabIndex="4"
+       title={props.title}
+       onClick={props.onClick}>
+      {icon(props)}
+    </a>
+  );
+}
+
+function className(props) {
+  return classNames('vjs-play-control',
+                    {'player_controls-play_button-custom_icon': !!props.iconName});
+}
+
+function icon(props) {
+  if (props.iconName) {
+    return (
+      <Icon name={props.iconName} />
+    );
+  }
+  else {
+    return (
+      <span />
+    );
+  }
+}
