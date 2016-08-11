d6e0d458caadbcf628e440ec0304c8f90365ca30
Tim Fischbach <tfischbach@codevise.de>
2016-08-11 11:50:28 +0200

    Add createModelResolver higher order Resolver function
    Allow defining resolvers that either take obtain props from Backbone
    collections or seed data.

---
+++ js/src/resolvers/createModelResolver.js
@@ -0,0 +1,80 @@
+import SeedResolver from 'resolvers/SeedResolver';
+import BackboneModelResolver from 'resolvers/BackboneModelResolver';
+
+/**
+ * Creates a resolver that
+ *
+ * - resolves props from seed data outside the editor.
+ * - resovles props from backbone models inside the editor.
+ *
+ * The resulting takes a `property` option, which determines the name
+ * of prop that contains the id of the model that shall be found.
+ *
+ * @param {createModelResolver~backboneCollection} backboneCollection
+ *   A function that returns the Backbone collection in which to
+ *   search for the models.
+ *
+ * @param {string} seedProperty
+ *   The name of the property which contains the list of models in the
+ *   seed data.
+ *
+ * @param {string} [options.idAttribute = 'id']
+ *   Name of the attribute that shall be used as the id of a
+ *   model. This is used to find the record either in the seed data or
+ *   the backbone collection.
+ *
+ * @param {string[]} [options.attributesForProps = ['id']]
+ *   List of model attributes that shall be included in the resolved
+ *   props.
+ *
+ * @param {boolean} [options.includeConfiguration = false]
+ *   If the model has a configuration attribute, include all of its
+ *   properties in the resolved props.
+ *
+ * @return {Class<Resolver>}
+ *
+ * @example
+ *
+ * resolve.register('page', createModelResolver({
+ *   backboneCollection: () => pageflow.pages,
+ *   seedProperty: 'pages',
+ *   idAttribute: 'perma_id',
+ *   attributesForProps: ['perma_id', 'chapter_id'],
+ *   includeConfiguration: true
+ * });
+ *
+ * createContainer(SomeComponent, {
+ *   fragments: {
+ *     linkedPage: resolve('page', {property: 'linkedPageId'})
+ *   }
+ * });
+ */
+export default function(options, {editorMode = PAGEFLOW_EDITOR} = {}) {
+  const commonResolverOptions = {
+    idAttribute: options.idAttribute || 'id',
+    attributesForProps: options.attributesForProps || ['id'],
+    includeConfiguration: options.includeConfiguration
+  };
+
+  return function({property}, callback) {
+    if (editorMode) {
+      return new BackboneModelResolver({
+        collection: options.backboneCollection,
+        property,
+        ...commonResolverOptions
+      }, callback);
+    }
+    else {
+      return new SeedResolver({
+        seedProperty: options.seedProperty,
+        property,
+        ...commonResolverOptions
+      }, callback);
+    }
+  };
+}
+
+/**
+ * @callback createModelResolver~backboneCollection
+ * @return {Backbone.Collection}
+ */
