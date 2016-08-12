9bae7e06244aabc79955c9483cff9f26a14bd842
Tim Fischbach <tfischbach@codevise.de>
2016-08-12 16:11:43 +0200

    Allow defining page thumbnail candidates

--- lib/pageflow-react.rb
+++ lib/pageflow-react.rb
@@ -2,8 +2,8 @@ require 'pageflow/react/engine'
 
 module Pageflow
   module React
-    def self.create_page_type(name, component_name)
-      Pageflow::React::PageType.new(name, component_name)
+    def self.create_page_type(name, component_name, options = {})
+      Pageflow::React::PageType.new(name, component_name, options)
     end
 
     def self.create_widget_type(name, role, component_name)
--- lib/pageflow/react/page_type.rb
+++ lib/pageflow/react/page_type.rb
@@ -3,13 +3,18 @@ module Pageflow
     class PageType < Pageflow::PageType
       attr_reader :name, :component_name
 
-      def initialize(name, component_name)
+      def initialize(name, component_name, options)
         @name = name
         @component_name = component_name
+        @thumbnail_candidates = options[:thumbnail_candidates]
       end
 
       def template_path
         'pageflow/react/page'
+      end
+
+      def thumbnail_candidates
+        @thumbnail_candidates || super
       end
     end
   end
