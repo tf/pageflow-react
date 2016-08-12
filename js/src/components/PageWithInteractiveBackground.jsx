e46d02ebd40b795a829097b00976e9ec7fb9303b
Tim Fischbach <tfischbach@codevise.de>
2016-08-12 15:40:16 +0200

    Add defaultControlBarText to PageWithInteractiveBackground

--- js/src/components/PageWithInteractiveBackground.jsx
+++ js/src/components/PageWithInteractiveBackground.jsx
@@ -72,7 +72,7 @@ class PageWithInteractiveBackground extends React.Component {
         <PageForeground>
           <PlayerControls playButtonTitle="Starten"
                           playButtonIconName={this.props.playButtonIconName}
-                          controlBarText={page.controlBarText}
+                          controlBarText={page.controlBarText || this.props.defaultControlBarText}
                           onPlayButtonClick={this.onPlayButtonClick}
                           infoBox={{title: page.additionalTitle, description: page.additionalDescription}} />
 
@@ -103,6 +103,7 @@ class PageWithInteractiveBackground extends React.Component {
 PageWithInteractiveBackground.propTypes = {
   page: React.PropTypes.object,
 
+  defaultControlBarText: React.PropTypes.string,
   playButtonIconName: React.PropTypes.string,
 
   onEnterBackground: React.PropTypes.func,
