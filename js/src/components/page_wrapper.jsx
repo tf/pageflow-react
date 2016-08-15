a93601543aefec98ac11839287ef8deb6d5a6da6
Tim Fischbach <tfischbach@codevise.de>
2016-08-15 15:59:39 +0200

    Allow setting custom css class on PageWrapper

--- js/src/components/page_wrapper.jsx
+++ js/src/components/page_wrapper.jsx
@@ -1,11 +1,12 @@
 import {Component} from 'react';
+import classNames from 'classnames';
 
 export default class extends Component {
   render() {
     return (
-      <div className="content_and_background">
+      <div className={classNames('content_and_background', this.props.className)}>
         {this.props.children}
       </div>
     );
   }
-};
+}
