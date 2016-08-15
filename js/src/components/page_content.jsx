716c531f85d88a5d48e9d88fed4adc4431d09bcb
Tim Fischbach <tfischbach@codevise.de>
2016-08-15 15:58:36 +0200

    Split PageContent into PageForeground and PageScroller

--- js/src/components/page_content.jsx
+++ js/src/components/page_content.jsx
@@ -1,51 +1,12 @@
-import React from 'react';
+import PageForeground from './page_foreground.jsx';
+import PageScroller from './page_scroller.jsx';
 
-import Scroller from './scroller';
-import createPageComponent from '../create_page_component.jsx';
-
-export default class PageContent extends React.Component {
-  static childContextTypes = {
-    pageScroller: React.PropTypes.object
-  }
-
-  getChildContext() {
-    this._pageScroller = this._pageScroller || {
-      disable: () => {
-        this.refs.scroller.disable();
-      },
-      enable: () => {
-        this.refs.scroller.enable();
-      }
-    };
-
-    return {
-      pageScroller: this._pageScroller,
-    };
-  }
-
-  render() {
-    return (
-      <div className="content">
-        <Scroller ref="scroller">
-          <div className="contentWrapper">
-            {this.props.children}
-          </div>
-        </Scroller>
-      </div>
-    );
-  }
-
-  pageWillActivate(options) {
-    this.refs.scroller.resetPosition({position: options.position})
-  }
-
-  pageDidActivate() {
-    this.refs.scroller.enable()
-  }
-
-  pageWillDeactivate() {
-    this.refs.scroller.disable()
-  }
-};
-
-export default createPageComponent(PageContent);
+export default function(props) {
+  return (
+    <PageForeground>
+      <PageScroller>
+        {props.children}
+      </PageScroller>
+    </PageForeground>
+  );
+}
