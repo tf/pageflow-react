0fdfcf3b54392795fdff8d573ea13b3877c976a9
Tim Fischbach <mail@timfischbach.de>
2016-11-22 09:29:23 +0100

    Allow object as disptachToProps in connectToPage

--- js/src/collections/createItemScopeConnector.jsx
+++ js/src/collections/createItemScopeConnector.jsx
@@ -2,6 +2,7 @@ import {ensureItemActionId} from './itemActionHelpers';
 import {addItemScope, getItemScopeProperty} from './itemScopeHelpers';
 
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import React from 'react';
 
 export default function(collectionName) {
@@ -13,11 +14,19 @@ export default function(collectionName) {
         mapStateToProps(
           addItemScope(state, collectionName, props[itemScopeProperty]), props
         ) : null,
-      mapDispatchToProps ? (dispatch, props) =>
-        mapDispatchToProps(action => {
+      mapDispatchToProps ? (dispatch, props) => {
+        const wrappedDispatch = function(action) {
           ensureItemActionId(action, collectionName, props[itemScopeProperty]);
           return dispatch(action);
-        }) : null,
+        };
+
+        if (typeof mapDispatchToProps == 'function') {
+          return mapDispatchToProps(wrappedDispatch, props);
+        }
+        else {
+          return bindActionCreators(mapDispatchToProps, wrappedDispatch);
+        }
+      } : null,
       mergeProps
     );
 
