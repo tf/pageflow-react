27e5a634d022c50d1172d2893dc1fa5d80269465
Tim Fischbach <tfischbach@codevise.de>
2016-11-24 14:02:05 +0100

    Update page type configuration editors

--- app/assets/javascripts/pageflow/react/editor.js
+++ app/assets/javascripts/pageflow/react/editor.js
@@ -5,7 +5,7 @@ pageflow.ConfigurationEditorView.register('plain', {
     });
 
     this.tab('files', function() {
-      this.input('background_image_id', pageflow.FileInputView, {collection: pageflow.videoFiles});
+      this.input('video_file_id', pageflow.FileInputView, {collection: pageflow.videoFiles});
       this.input('poster_image_id', pageflow.FileInputView, {collection: pageflow.imageFiles});
       this.input('thumbnail_image_id', pageflow.FileInputView, {
         collection: pageflow.imageFiles,
@@ -27,11 +27,20 @@ pageflow.ConfigurationEditorView.register('new_video', {
   configure: function() {
     this.tab('general', function() {
       this.group('general');
+
+      this.input('additional_title', pageflow.TextInputView);
+      this.input('additional_description', pageflow.TextAreaInputView, {size: 'short'});
     });
 
     this.tab('files', function() {
-      this.input('video_file_id', pageflow.FileInputView, {collection: pageflow.videoFiles});
-      this.input('poster_image_id', pageflow.FileInputView, {collection: pageflow.imageFiles});
+      this.input('video_file_id', pageflow.FileInputView, {
+        collection: pageflow.videoFiles,
+        positioning: false
+      });
+      this.input('poster_image_id', pageflow.FileInputView, {
+        collection: pageflow.imageFiles,
+        positioning: false
+      });
       this.input('thumbnail_image_id', pageflow.FileInputView, {
         collection: pageflow.imageFiles,
         positioning: false
@@ -43,7 +52,13 @@ pageflow.ConfigurationEditorView.register('new_video', {
     });
 
     this.tab('options', function() {
-      this.group('options');
+      this.input('autoplay', pageflow.CheckBoxInputView);
+
+      if (pageflow.features.isEnabled('auto_change_page')) {
+        this.input('auto_change_page_on_ended', pageflow.CheckBoxInputView);
+      }
+
+      this.group('options', {canPauseAtmo: true});
     });
   }
 });
