460f7334bb0e945b3386c38296fefcca385c7761
Tim Fischbach <tfischbach@codevise.de>
2016-12-08 17:48:57 +0100

    wip built in page types

--- js/src/builtInPageTypes/index.js
+++ js/src/builtInPageTypes/index.js
@@ -1,7 +1,9 @@
+import {register as registerAudioPage} from './AudioPage';
 import {register as registerPlainPage} from './PlainPage';
 import {register as registerVideoPage} from './videoPageType';
 
 export function register() {
   registerPlainPage();
   registerVideoPage();
+  registerAudioPage();
 }
