ee4db7cb367b2dcf353f2764dea64a43c10ec376
Tim Fischbach <tfischbach@codevise.de>
2016-12-08 17:47:16 +0100

    Export PageBackgroundVideo from media module

--- js/src/media/components/PageBackgroundVideo.jsx
+++ js/src/media/components/PageBackgroundVideo.jsx
@@ -2,6 +2,6 @@ import PageVideoPlayer from './PageVideoPlayer';
 
 export default function PageBackgroundVideo(props) {
   return (
-    <PageVideoPlayer loop={true} {...props} />
+    <PageVideoPlayer loop={true} textTracksEnabled={false} {...props} />
   );
 }
