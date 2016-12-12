566cd7448dbe5fe10485e2b8497cd6064d9e706e
Tim Fischbach <tfischbach@codevise.de>
2016-12-12 10:32:38 +0100

    Allow toggling media playback via hotkey

--- js/src/media/sagas/togglePlaying.js
+++ js/src/media/sagas/togglePlaying.js
@@ -1,11 +1,12 @@
 import {TOGGLE_PLAYING, play, pause} from '../actions';
+import {HOTKEY_SPACE} from 'hotkeys/actions';
 import {playerState} from '../selectors';
 
 import {takeEvery} from 'redux-saga';
 import {put, select} from 'redux-saga/effects';
 
 export default function*() {
-  yield takeEvery(TOGGLE_PLAYING, toggle);
+  yield takeEvery([HOTKEY_SPACE, TOGGLE_PLAYING], toggle);
 }
 
 function* toggle() {
