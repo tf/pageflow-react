520f6f17d3a03fe86096d50659540b3142b41870
Tim Fischbach <tfischbach@codevise.de>
2016-12-22 20:44:04 +0100

    Set NODE_ENV for production

--- js/Gruntfile.js
+++ js/Gruntfile.js
@@ -1,12 +1,22 @@
 /*global require, module*/
 
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+
+webpackConfig.plugins = webpackConfig.plugins || [];
+webpackConfig.plugins.push(new webpack.DefinePlugin({
+  'process.env': {
+    'NODE_ENV': JSON.stringify('production')
+  }
+}));
+
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-webpack');
   grunt.loadNpmTasks('grunt-karma');
 
   grunt.initConfig({
     webpack: {
-      options: require('./webpack.config.js'),
+      options: webpackConfig,
       build: {}
     },
 
