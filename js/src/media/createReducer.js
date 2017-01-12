332578d8069d64ed3b7601f595dd82d9f9e01384
Tim Fischbach <tfischbach@codevise.de>
2017-01-12 12:57:59 +0100

    fixup! Ensure video plays in native player on Android Phone

--- js/src/media/createReducer.js
+++ js/src/media/createReducer.js
@@ -1,5 +1,5 @@
 import {
-  PLAY, PAUSE, PAUSED, SEEK_TO,
+  PLAY, PAUSE, SEEK_TO,
   FADE_OUT_AND_PAUSE, PLAY_AND_FADE_IN,
   PREBUFFER, PREBUFFERED,
   BUFFER_UNDERRUN, WAITING, SEEKING, SEEKED,
@@ -54,7 +54,6 @@ export default function({scope = 'default'} = {}) {
         fadeDuration: action.payload.fadeDuration,
         isLoading: true
       };
-    case PAUSED:
     case PAUSE:
       return {
         ...state,
