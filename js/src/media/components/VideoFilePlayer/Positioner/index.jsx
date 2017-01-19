b522d71c5fe77856fbd3d8d462f8f91829d4607a
Tim Fischbach <tfischbach@codevise.de>
2017-01-19 18:46:51 +0100

    Use react-measure for positioner

--- js/src/media/components/VideoFilePlayer/Positioner/index.jsx
+++ js/src/media/components/VideoFilePlayer/Positioner/index.jsx
@@ -1,71 +1,37 @@
 import getDimensions from './getDimensions';
 import getCueOffsetClassName from './getCueOffsetClassName';
 
-import {connectInPage} from 'pages';
-import {pageIsActive} from 'pages/selectors';
-import {combine} from 'utils';
-
 import React from 'react';
+import Measure from 'react-measure';
 
-export class Positioner extends React.Component {
-  constructor(props, state) {
-    super(props, state);
-    this.state = {};
-
-    this.bindWrapper = wrapper => {
-      this.wrapper = wrapper;
-
-      if (wrapper) {
-        pageflow.events.on('resize', this.measure);
-        this.measure();
-      }
-      else {
-        pageflow.events.off('resize', this.measure);
-      }
-    };
-
-    this.measure = () => {
-      this.setState({
-        wrapperDimensions: {
-          width: this.wrapper.clientWidth,
-          height: this.wrapper.clientHeight
-        }
-      });
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (!this.props.pageIsActive && nextProps.pageIsActive) {
-      this.measure();
-    }
-  }
-
-  render() {
-    const dimensions = getDimensions(this.props.videoFile,
-                                     this.props.fit,
-                                     this.props.position,
-                                     this.state.wrapperDimensions);
-
-    return (
-      <div ref={this.bindWrapper} className="videoWrapper">
-        <div className={getCueOffsetClassName(dimensions, this.state.wrapperDimensions)}
-             style={style(dimensions)}>
-          {this.props.children}
-        </div>
-      </div>
-    );
-  }
+export default function Positioner(props) {
+  return (
+    <Measure whitelist={['width', 'height']}>
+      {wrapperDimensions => renderWrapper(props, wrapperDimensions)}
+    </Measure>
+  );
 }
-
-export default connectInPage(combine({
-  pageIsActive: pageIsActive()
-}))(Positioner);
 
 Positioner.propTypes = {
   videoFile: React.PropTypes.object,
   fit: React.PropTypes.oneOf(['contain', 'cover', 'smart_contain']),
   position: React.PropTypes.arrayOf(React.PropTypes.number)
 };
+
+function renderWrapper(props, wrapperDimensions) {
+  const dimensions = getDimensions(props.videoFile,
+                                   props.fit,
+                                   props.position,
+                                   wrapperDimensions);
+  return (
+    <div className="videoWrapper">
+      <div className={getCueOffsetClassName(dimensions, wrapperDimensions)}
+           style={style(dimensions)}>
+        {props.children}
+      </div>
+    </div>
+  );
+}
 
 function style(dimensions) {
   return dimensions && {
